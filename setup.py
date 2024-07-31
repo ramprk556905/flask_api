24b8fc60edd758c0e3374e892735d29481ea05db
DAI\ramkumar.pandidurai <ramkumar.pandidurai>
2024-07-31 19:01:14 +0530

    Add setup.py for conditional dependencies

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import platform
+
+install_requires = [
+    'flask',
+    'flask_sqlalchemy',
+    'flask_login',
+    'flasgger',
+    # Add other common dependencies here
+]
+
+if platform.system() == 'Windows':
+    install_requires.append('pywin32==306')
+
+setup(
+    name='flask_app',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+)
